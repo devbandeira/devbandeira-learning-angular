7339973fdff6d38670e0945da7aa04f3582d4f20
devbandeira <dev.bandeira@outlook.com>
2022-12-09 23:30:58 -0300

    Renderizando Listas

--- src/app/rendenrizando-listas/rendenrizando-listas.component.ts
+++ src/app/rendenrizando-listas/rendenrizando-listas.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Celular } from '../Types/Celular';
 
 @Component({
   selector: 'app-rendenrizando-listas',
   templateUrl: './rendenrizando-listas.component.html',
-  styleUrls: ['./rendenrizando-listas.component.css']
+  styleUrls: ['./rendenrizando-listas.component.css'],
 })
 export class RendenrizandoListasComponent {
-
+  celulares: Celular[] = [
+    {
+      id: 1,
+      nome: 'Celular XL',
+      descricao: 'Um celular grande',
+      esgotado: false,
+    },
+    {
+      id: 2,
+      nome: 'Celular Normal',
+      esgotado: false,
+    },
+    {
+      id: 3,
+      nome: 'Celular Mini',
+      descricao: 'Um celular pequeno',
+      esgotado: true,
+    },
+  ];
 }
